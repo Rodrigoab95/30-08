747062ec5a25e2932650456e3b921e0d6cebbf38
Rodrigoab95 <rodrigo.abarreira@gmail.com>
2024-08-30 20:45:00 -0300

    aprendendo funções em python

---
+++ def.py
@@ -0,0 +1,22 @@
+import os
+
+def limpar_tela():
+    os.system('cls')
+
+def ler_numero():
+    try:
+        return int(input("Informe um valor: "))
+    except ValueError as erro:
+        print("Informe um número!")
+        return ler_numero()
+
+def soma(n1, n2):
+    return n1+n2
+
+def main():
+    limpar_tela()
+    n1 = ler_numero()
+    n2 = ler_numero()
+    print(soma(n1,n2))
+
+main()
