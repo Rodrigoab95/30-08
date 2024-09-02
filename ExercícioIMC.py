0ac45133f2e3c7ee0403ec0ed3308a4730181d1e
Rodrigoab95 <93782190+Rodrigoab95@users.noreply.github.com>
2024-09-02 00:15:40 -0300

    Finalizando exercício IMC

--- ExercícioIMC.py
+++ ExercícioIMC.py
@@ -1,8 +1,20 @@
-import os
+def calcular_imc():
+    try:
+        peso = float(input("Digite o peso da pessoa em quilogramas: "))
+        altura = float(input("Digite a altura da pessoa em metros: "))
+        
+        if peso <= 0 or altura <= 0:
+            raise ValueError("O peso e a altura devem ser valores positivos.")
+        
+        imc = peso / (altura ** 2)
+        print("O IMC da pessoa é: {:.2f}".format(imc))
+        
+    except ValueError as e:
+        print("Erro: {}".format(e))
+    
+    finally:
+        continuar = input("Deseja continuar? (s/n): ")
+        if continuar.lower() == 's':
+            calcular_imc()
 
-os.system('cls')
-
-peso = 0.0
-altura = 0
-
-imc = peso / (altura * altura)
+calcular_imc()
